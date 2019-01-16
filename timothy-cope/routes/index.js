060640e44c2931c35d302493e866e847ae84de1a
Tim Cope <kirbycope@gmail.com>
2019-01-16 15:00:04 -0700

    Initial commit

---
+++ timothy-cope/routes/index.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+// Load the external dependencies (from package.json)
+var express = require('express');
+var router = express.Router();
+
+// Home page
+router.get('/', function (req, res) {
+    res.render('index', {
+        static_path: 'public',
+        theme: process.env.THEME || 'default',
+        flask_debug: process.env.FLASK_DEBUG || 'false'
+    });
+});
+
+module.exports = router;

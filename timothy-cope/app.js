43456357e483974e54c5609be38650c873b3e9dd
Tim Cope <kirbycope@gmail.com>
2021-03-28 11:05:28 -0700

    Update app.js
    Added a traffic logger that logs time and path to a DynamoDB called traffic on AWS.

--- timothy-cope/app.js
+++ timothy-cope/app.js
@@ -4,7 +4,6 @@ var debug = require('debug');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -17,17 +16,55 @@ var blog = require('./routes/blog');
 var reviewsApi = require('./routes/api/reviews');
 var reviews = require('./routes/reviews');
 
+// Amazon Web Services config
+var AWS = require("aws-sdk");
+AWS.config.loadFromPath('./config.json');
+AWS.config.update({ endpoint: "https://dynamodb.us-east-1.amazonaws.com" });
+var docClient = new AWS.DynamoDB.DocumentClient();
+var tableName = "traffic";
+
 // Create an Express application
 var app = express();
+
+// Create log mechanism
+var logger = function (req, res, next) {
+    var originalURL = req.originalUrl;
+    if (originalURL.indexOf('/public/') === -1) {
+        console.log(originalURL);
+        // Define DB item
+        var item = {
+            date: new Date().getTime(),
+            path: originalURL
+        };
+        // DynamoDB Object
+        var params = {
+            TableName: tableName,
+            Item: item
+        };
+        // POST the Object to the DataBase
+        docClient.put(params, function (err, data) {
+            // If the DB request returned an error
+            if (err) {
+                // Return the error to the user
+                console.log(err);
+                next();
+            }
+            else {
+                next();
+            }
+        });
+    }
+    else {
+        next();
+    }
+}
+app.use(logger);
 
 // Set the View Engine for the Express application
 app.set('view engine', 'ejs');
 app.use('/views', express.static(path.resolve(__dirname, 'views')));
 app.use('/public', express.static(path.resolve(__dirname, 'public')));
-
-// uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -42,14 +79,12 @@ app.use('/blog', blog);
 app.use('/reviews', reviews);
 app.use('/api/reviews', reviewsApi);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
-// error handlers
 
 // development error handler
 // will print stacktrace

8ca1941394eab1b46758b92393a3c2ac1685bbd8
Timothy Cope <kirbycope@gmail.com>
2023-02-25 15:56:22 -0800

    Crispy

--- app.js
+++ app.js
@@ -4,25 +4,9 @@ var debug = require('debug');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 // Load the internal dependencies (.js files)
 var index = require('./routes/index');
-var adminApi = require('./routes/api/admin');
-var admin = require('./routes/admin');
-var blogApi = require('./routes/api/blog');
-var blog = require('./routes/blog');
-var reviewsApi = require('./routes/api/reviews');
-var reviews = require('./routes/reviews');
-
-// Amazon Web Services config
-var AWS = require("aws-sdk");
-const { options } = require('./routes/index');
-AWS.config.loadFromPath('./config.json');
-AWS.config.update({ endpoint: "https://dynamodb.us-east-1.amazonaws.com" });
-var docClient = new AWS.DynamoDB.DocumentClient();
-var tableName = "traffic";
 
 // Create an Express application
 var app = express();
@@ -138,29 +122,7 @@ var logger = function (req, res, next) {
             setTimeout(() => next(), 31337);
         }
         else {
-            console.log();
-            // Define DB item
-            var item = {
-                date: new Date().getTime(),
-                path: originalURL
-            };
-            // DynamoDB Object
-            var params = {
-                TableName: tableName,
-                Item: item
-            };
-            // POST the Object to the DataBase
-            docClient.put(params, function (err, data) {
-                // If the DB request returned an error
-                if (err) {
-                    // Return the error to the user
-                    console.log(err);
-                    next();
-                }
-                else {
-                    next();
-                }
-            });
+            next();
         }
     }
     else {
@@ -174,19 +136,10 @@ app.set('view engine', 'ejs');
 app.use('/views', express.static(path.resolve(__dirname, 'views')));
 app.use('/public', express.static(path.resolve(__dirname, 'public')));
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Setup web app routes (controllers)
 app.use('/', index);
-app.use('/api/admin', adminApi);
-app.use('/admin', admin);
-app.use('/api/blog', blogApi);
-app.use('/blog', blog);
-app.use('/reviews', reviews);
-app.use('/api/reviews', reviewsApi);
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- public/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-﻿// After the window has loaded...
-window.onload = function () {
-
-    // <!-- Set the 'active' aside link -->
-    var asideDashboardLink = document.getElementById('asideDashboardLink');
-    asideDashboardLink.classList.add('active');
-
-};
